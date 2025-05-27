b054c27ac2ea12aa2db86de09b742315f51f84ee
Tarfprima <granta.150@gmail.com>
2025-05-27 14:46:58 +0300

    Обновлено manage.py, admin, html файл

--- internet_shop/internet_shop/settings.py
+++ internet_shop/internet_shop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'internet_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'shop', # имя бд
+        'USER': 'manager', # имя пользователя
+        'PASSWORD': 'warfare888',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- internet_shop/product/models.py
+++ internet_shop/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    timer = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
